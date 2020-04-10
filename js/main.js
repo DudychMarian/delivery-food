de08353b7f6fd862d146bc31d6841889a0b9443d
Marian <dudychmarian@gmail.com>
2020-04-10 12:28:23 +0300

    Upload project

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const cartButton = document.querySelector("#cart-button");
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
+
+cartButton.addEventListener("click", toggleModal);
+close.addEventListener("click", toggleModal);
+
+function toggleModal() {
+  modal.classList.toggle("is-open");
+}
+
+new WOW().init();
